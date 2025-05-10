57c5b2df7d493842fa4549ce810d835313bcf40c
lubie-koty <art.zelazko@gmail.com>
2025-05-10 12:29:42 +0200

    service logic implementation

--- internal/base/base.go
+++ internal/base/base.go
@@ -1,32 +1,46 @@
 package base
 
-type CombinedMathService struct{}
+import "github.com/lubie-koty/rpc-compute-service-combined/internal/core/types"
 
-func NewCombinedMathService() *CombinedMathService {
-	return &CombinedMathService{}
+type CombinedMathService struct {
+	simpleService  types.SimpleServiceClient
+	complexService types.ComplexServiceClient
+}
+
+func NewCombinedMathService(simpleService types.SimpleServiceClient, complexService types.ComplexServiceClient) *CombinedMathService {
+	return &CombinedMathService{
+		simpleService:  simpleService,
+		complexService: complexService,
+	}
 }
 
 func (s *CombinedMathService) RootMeanSquare(firstValue float64, secondValue float64) float64 {
-	// TODO
-	return 0
+	xPower := s.complexService.Power(firstValue, 2)
+	yPower := s.complexService.Power(secondValue, 2)
+	sum := s.simpleService.Add(xPower, yPower)
+	avg := s.simpleService.Div(sum, 2)
+	return s.complexService.Sqrt(avg)
 }
 
 func (s *CombinedMathService) GeometricMean(firstValue float64, secondValue float64) float64 {
-	// TODO
-	return 0
+	product := s.simpleService.Mul(firstValue, secondValue)
+	return s.complexService.Sqrt(product)
 }
 
-func (s *CombinedMathService) BodyMassIndex(firstValue float64, secondValue float64) float64 {
-	// TODO
-	return 0
+func (s *CombinedMathService) BodyMassIndex(weight float64, height float64) float64 {
+	heightPower := s.complexService.Power(height, 2)
+	return s.simpleService.Div(weight, heightPower)
 }
 
 func (s *CombinedMathService) PowerLevelDiff(firstValue float64, secondValue float64) float64 {
-	// TODO
-	return 0
+	ratio := s.simpleService.Div(firstValue, secondValue)
+	logValue := s.complexService.Log(ratio, 10)
+	return s.simpleService.Mul(10, logValue)
 }
 
 func (s *CombinedMathService) PercentageValueChange(firstValue float64, secondValue float64) float64 {
-	// TODO
-	return 0
+	delta := s.simpleService.Sub(secondValue, firstValue)
+	div := s.simpleService.Div(delta, s.complexService.Abs(firstValue))
+	percentage := s.simpleService.Mul(div, 100)
+	return s.complexService.Round(percentage, 2)
 }
--- internal/core/types/types.go
+++ internal/core/types/types.go
@@ -7,7 +7,22 @@ type Server interface {
 type MathService interface {
 	RootMeanSquare(firstValue float64, secondValue float64) float64
 	GeometricMean(firstValue float64, secondValue float64) float64
-	BodyMassIndex(firstValue float64, secondValue float64) float64
+	BodyMassIndex(weight float64, height float64) float64
 	PowerLevelDiff(firstValue float64, secondValue float64) float64
 	PercentageValueChange(firstValue float64, secondValue float64) float64
+}
+
+type SimpleServiceClient interface {
+	Add(a, b float64) float64
+	Sub(a, b float64) float64
+	Mul(a, b float64) float64
+	Div(a, b float64) float64
+}
+
+type ComplexServiceClient interface {
+	Sqrt(num float64) float64
+	Abs(num float64) float64
+	Power(base float64, exponent float64) float64
+	Log(num float64, base float64) float64
+	Round(num float64, precision int64) float64
 }
